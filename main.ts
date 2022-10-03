54d17b163007d23dd5201cc11486f00a46c58246
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-10-03 19:23:01 +0200

    Mejoro luces. Impacto 3g

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.ThreeG, function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 125)
     basic.pause(1000)
 })
@@ -6,7 +6,10 @@ basic.showIcon(IconNames.Happy)
 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 for (let index = 0; index < 12; index++) {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     basic.pause(200)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     basic.pause(200)
 }
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
